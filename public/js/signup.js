0bc052037b6c670466407a99c6d979a866726634
Matthew Duffield <duffieldml@gmail.com>
2021-03-16 19:22:07 -0400

    Added ability to capture info from the signup page

--- public/js/signup.js
+++ public/js/signup.js
@@ -2,33 +2,50 @@ $(document).ready(() => {
   // Getting references to our form and input
   const signUpForm = $("form.signup");
   const emailInput = $("input#email-input");
-  const passwordInput = $("input#password-input");
+  const firstNameInput = $("input#first-input");
+  const lastNameInput = $("input#last-input");
+  const stateInput = $("input#state-input");
+  const ageInput = $("input#age-input");
+  // const essentialInput = $("input#yes-input");
+  
 
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", event => {
     event.preventDefault();
     const userData = {
       email: emailInput.val().trim(),
-      password: passwordInput.val().trim()
+      first_name: firstNameInput.val().trim(),
+      last_name: lastNameInput.val().trim(),
+      state: stateInput.val().trim(),
+      age: ageInput.val().trim(),
+      // essential_worker: essentialInput.val(),
     };
 
-    if (!userData.email || !userData.password) {
+    if (!userData.email || !userData.first_name || !userData.last_name || !userData.state || !userData.age) {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.password);
+    signUpUser(userData.email, userData.first_name, userData.last_name, userData.state, userData.age);
     emailInput.val("");
-    passwordInput.val("");
+    firstNameInput.val("");
+    lastNameInput.val("");
+    stateInput.val("");
+    ageInput.val("");
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
   // Otherwise we log any errors
-  function signUpUser(email, password) {
+  function signUpUser(email, first_name, last_name, state, age, essential_worker) {
     $.post("/api/signup", {
       email: email,
-      password: password
+      first_name: first_name,
+      last_name: last_name,
+      state: state,
+      age: age
+      // essential_worker: essential_worker
     })
       .then(() => {
+        //make this redirect to a waiting list or something
         window.location.replace("/members");
         // If there's an error, handle it by throwing up a bootstrap alert
       })
