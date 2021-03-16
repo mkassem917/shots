c619e669218c2839b85f8d8d5bb1431d24fa3f0d
vshulman25 <victoriashulman25@gmail.com>
2021-03-15 21:31:16 -0400

    state change

--- models/user.js
+++ models/user.js
@@ -25,7 +25,7 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.STRING,
       allowNull: false
     },
-    location: {
+    state: {
       type: DataTypes.STRING,
       allowNull: false
     },
