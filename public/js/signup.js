36efc64bbabb1162403658c8b4f3651a5d1bfac3
Matthew Duffield <duffieldml@gmail.com>
2021-03-16 21:05:14 -0400

    Fixed essential worker issue

--- public/js/signup.js
+++ public/js/signup.js
@@ -4,10 +4,9 @@ $(document).ready(() => {
   const emailInput = $("input#email-input");
   const firstNameInput = $("input#first-input");
   const lastNameInput = $("input#last-input");
-  const stateInput = $("input#state-input");
+  const stateInput = $("#state-input");
   const ageInput = $("input#age-input");
-  // const essentialInput = $("input#yes-input");
-  
+  const essentialInput = $("#essential-input");
 
   // When the signup button is clicked, we validate the email and password are not blank
   signUpForm.on("submit", event => {
@@ -18,19 +17,21 @@ $(document).ready(() => {
       last_name: lastNameInput.val().trim(),
       state: stateInput.val().trim(),
       age: ageInput.val().trim(),
-      // essential_worker: essentialInput.val(),
+      essential_worker: essentialInput.val().trim(),
     };
 
     if (!userData.email || !userData.first_name || !userData.last_name || !userData.state || !userData.age) {
       return;
     }
     // If we have an email and password, run the signUpUser function
-    signUpUser(userData.email, userData.first_name, userData.last_name, userData.state, userData.age);
+    signUpUser(userData.email, userData.first_name, userData.last_name, userData.state, userData.age, userData.essential_worker);
     emailInput.val("");
     firstNameInput.val("");
     lastNameInput.val("");
     stateInput.val("");
     ageInput.val("");
+    essentialInput.val("");
+    console.log(userData);
   });
 
   // Does a post to the signup route. If successful, we are redirected to the members page
@@ -41,12 +42,12 @@ $(document).ready(() => {
       first_name: first_name,
       last_name: last_name,
       state: state,
-      age: age
-      // essential_worker: essential_worker
+      age: age,
+      essential_worker: essential_worker
     })
       .then(() => {
         //make this redirect to a waiting list or something
-        window.location.replace("/members");
+        window.location.replace("https://www.google.com/");
         // If there's an error, handle it by throwing up a bootstrap alert
       })
       .catch(handleLoginErr);
