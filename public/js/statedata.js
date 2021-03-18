48ec57636aded138565d3cf7b872fa2f6eb878ee
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-03-17 21:57:50 -0400

    apiroute for covidnow
    NEED: set up key for map itself with process env var.
    WORKING: covidnow api route link logs in consle and will pass in the state.
    NEED to link userid to a on click function

--- public/js/statedata.js
+++ public/js/statedata.js
@@ -5,16 +5,23 @@
 //     if (userId) {
 //       userId = `/?user_id=${1}`;
 //     }
-//let userId = "1";
+const userId = "1";
 
-// fetch(`/api/user/${userId}`, {
-//   method: "GET",
-//   headers: {
-//     "Content-Type": "application/json",
-//   },
-// })
-//   .then((response) => response.json())
-//   .then((data) => {
-//     console.log("Success in getting posts:", data);
-//   });
+// NEED to set up a key for the map itself with process.env variable to pass through the map
+//if you click on the map link in the console log, you will get the api data for the sate
+fetch(`/api/user/${userId}`, {
+  method: "GET",
+  headers: {
+    "Content-Type": "application/json",
+  },
+})
+  .then((response) => response.json())
+  .then((data) => {
+    console.log("Success in getting posts:", data);
+    const state = data.state; 
+    console.log(state); 
+    const map = `https://api.covidactnow.org/v2/state/${state}.json?apiKey=7e3bafb672774e49bfa657f1d33be17a`;
+    console.log(map); 
+  });
 // //}
+
