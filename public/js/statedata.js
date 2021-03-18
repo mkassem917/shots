57ffba0ea058a7c65ecb576ed42515de2051279f
Moe Kassem <74279957+mkassem917@users.noreply.github.com>
2021-03-17 21:06:35 -0400

    Update statedata.js

--- public/js/statedata.js
+++ public/js/statedata.js
@@ -5,16 +5,16 @@
 //     if (userId) {
 //       userId = `/?user_id=${1}`;
 //     }
-let userId = "1"; 
+//let userId = "1";
 
-    fetch(`/api/user/${userId}`, {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log('Success in getting posts:', data);
-  })
-//}
+// fetch(`/api/user/${userId}`, {
+//   method: "GET",
+//   headers: {
+//     "Content-Type": "application/json",
+//   },
+// })
+//   .then((response) => response.json())
+//   .then((data) => {
+//     console.log("Success in getting posts:", data);
+//   });
+// //}
