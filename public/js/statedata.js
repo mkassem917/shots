a7431515aea1aa7fdfb9e0ad9819292fbc3d992d
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-03-18 18:51:23 -0400

    updates to data pulled and displayed
    pulls more information and displays more from the api

--- public/js/statedata.js
+++ public/js/statedata.js
@@ -19,17 +19,35 @@ fetch(`/api/user/${userId}`, {
   .then((data) => {
     console.log("Success in getting posts:", data);
     const state = data.state;
-    console.log(state);
-    const map =
+    const user = data.first_name;
+    const stateData =
       `https://api.covidactnow.org/v2/state/${state}.json?apiKey=` + key;
-    console.log(map);
+    console.log(stateData);
+    $("#userWelcome").append(user); 
 
     $.ajax({
-      url: map,
+      url: stateData,
       method: "GET",
     }).then((response) => {
       console.log(response.url);
+      $("#userState").append(response.state);
       $("#cases").append(response.actuals.cases);
+
+      if (response.riskLevels.overall === "2" || "3" || "4" || "5") {
+        $("#riskLevelHigh").append("Critical to Extreme Risk Level");
+        $("#riskLevelLow").hide();
+        $("#riskLevelMed").hide();
+      }
+      if (response.riskLevels.overall === "0") {
+        $("#riskLevelHigh").hide();
+        $("#riskLevelLow").append("Critical to Extreme Risk Level");
+        $("#riskLevelMed").hide();
+      }
+      if (response.riskLevels.overall === "1") {
+        $("#riskLevelHigh").hide();
+        $("#riskLevelLow").hide();
+        $("#riskLevelMed").append("Critical to Extreme Risk Level");
+      }
     });
   });
 
