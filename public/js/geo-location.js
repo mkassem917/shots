2f5a20fd193cca3a6189075bc4567dd17d123ea0
Moe Kassem <74279957+mkassem917@users.noreply.github.com>
2021-03-18 15:04:42 -0400

    updated geo-location code
    added API Key from Google Console

--- public/js/geo-location.js
+++ public/js/geo-location.js
@@ -1,33 +1,52 @@
 
-function geoFindMe() {
+        $(document).ready(function () {
+            if ("geolocation" in navigator) {
+                navigator.geolocation.getCurrentPosition(function (p) {
+                    showUserDetails(p.coords.latitude, p.coords.longitude);
+                }, function (e) {
+                    ipLookup();
+                });
+            } else
+                ipLookup();
+        });
 
-    const status = document.querySelector('#status');
-    const mapLink = document.querySelector('#map-link');
-  
-    mapLink.href = '';
-    mapLink.textContent = '';
-  
-    function success(position) {
-      const latitude  = position.coords.latitude;
-      const longitude = position.coords.longitude;
-  
-      status.textContent = '';
-      mapLink.href = `https://www.openstreetmap.org/#map=18/${latitude}/${longitude}`;
-      mapLink.textContent = `Latitude: ${latitude} °, Longitude: ${longitude} °`;
-    }
-  
-    function error() {
-      status.textContent = 'Unable to retrieve your location';
-    }
-  
-    if(!navigator.geolocation) {
-      status.textContent = 'Geolocation is not supported by your browser';
-    } else {
-      status.textContent = 'Locating…';
-      navigator.geolocation.getCurrentPosition(success, error);
-    }
-  
-  }
-  
-  document.querySelector('#find-me').addEventListener('click', geoFindMe);
+        function showUserDetails(latitude, longitude, additional) {
+            var position = latitude + "," + longitude;
 
+            $("#latitude").text(latitude);
+            $("#longitude").text(longitude);
+
+            var url = "https://www.google.com/maps/embed/v1/view?center="+position+"&zoom=12&key=AIzaSyBDHRQ1mpF_Jp1SKvGZXgxlnaYuD4pmA2I";
+            $("iframe").attr('src', url);
+
+            if (typeof additional != "undefined") {
+                $("#country").text(additional.country.name);
+                $("#city").text(additional.city.name);
+                $("#continent").text(additional.continent.name);
+            }
+        }
+
+        function ipLookup() {
+            $.get('https://api.userinfo.io/userinfos', function (r) {
+               showUserDetails(r.position.latitude, r.position.longitude, r);
+            });
+        }
+
+
+
+            // $('#location-button').click(function(){
+        
+          //   if (navigator.geolocation) {
+          //       navigator.geolocation.getCurrentPosition(function(position){
+          //         console.log(position);
+          //         $.get( "http://maps.googleapis.com/maps/api/geocode/json?latlng="+ position.coords.latitude + "," + position.coords.longitude +"&sensor=false", function(data) {
+          //           console.log(data);
+          //         })
+          //         var img = new Image();
+          //         img.src = "https://maps.googleapis.com/maps/api/staticmap?center=" + position.coords.latitude + "," + position.coords.longitude + "&zoom=13&size=800x400&sensor=false";
+          //         $('#output').html(img);
+          //       });
+                
+          //   }
+
+          // });
