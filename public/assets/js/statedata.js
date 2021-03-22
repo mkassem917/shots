475764dfbb7864b70143c5d6cf348e92ab968195
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-03-21 22:02:59 -0400

    geolocation
    added console.logs in the function to try and track what was happening
    
    the bottom two functions aren't being read for me.

--- public/assets/js/statedata.js
+++ public/assets/js/statedata.js
@@ -1,36 +1,52 @@
-$(document).ready(() => {
-  const key = "7e3bafb672774e49bfa657f1d33be17a";
+// $(document).ready(() => {
+//   console.log(userState);
+//   const stateData =
+//     `https://api.covidactnow.org/v2/state/${userState}.json?apiKey=` + key;
+//   console.log(stateData);
 
-  // node fetch
-  const userState = $("#state-input"); 
-  console.log(userState);
-  const stateData =
-    `https://api.covidactnow.org/v2/state/${userState}.json?apiKey=` + key;
-  
+//   $.ajax({
+//     url: stateData,
+//     method: "GET",
+//   }).then((response) => {
+//     console.log(response.url);
+//     $("#userState").append(response.state);
 
-  $.ajax({
-    url: stateData,
-    method: "GET",
-  }).then((response) => {
-    console.log(response.url);
-    $("#userState").append(response.state);
-    $("#cases").append(response.actuals.cases);
+//     if (response.riskLevels.overall === "0") {
+//       $("#one").hide();
+//       $("#twothree").hide();
+//       $("#four").hide();
+//       $("#five").hide();
+//       $("#level").append("LOW");
+//     }
+//     if (response.riskLevels.overall === "1") {
+//       $("#zero").hide();
+//       $("#twothree").hide();
+//       $("#four").hide();
+//       $("#five").hide();
+//       $("#level").append("MEDIUM");
+//     }
+//     if (response.riskLevels.overall === "2" || "3") {
+//       $("#zero").hide();
+//       $("#one").hide();
+//       $("#four").hide();
+//       $("#five").hide();
+//       $("#level").append("HIGH");
+//     }
+//     if (response.riskLevels.overall === "4") {
+//       $("#zero").hide();
+//       $("#one").hide();
+//       $("#twothree").hide();
+//       $("#five").hide();
+//       $("#level").append("a VERY HIGH ");
+//     }
+//     if (response.riskLevels.overall === "5") {
+//       $("#zero").hide();
+//       $("#one").hide();
+//       $("#twothree").hide();
+//       $("#five").hide();
+//       $("#level").append("SEVERE");
+//     }
 
-    if (response.riskLevels.overall === "2" || "3" || "4" || "5") {
-      $("#riskLevelHigh").append("Critical to Extreme Risk Level");
-      $("#riskLevelLow").hide();
-      $("#riskLevelMed").hide();
-    }
-    if (response.riskLevels.overall === "0") {
-      $("#riskLevelHigh").hide();
-      $("#riskLevelLow").append("Critical to Extreme Risk Level");
-      $("#riskLevelMed").hide();
-    }
-    if (response.riskLevels.overall === "1") {
-      $("#riskLevelHigh").hide();
-      $("#riskLevelLow").hide();
-      $("#riskLevelMed").append("Critical to Extreme Risk Level");
-    }
-  });
-
-});
+//     $("#cases").append(response.actuals.cases);
+//   });
+// });
