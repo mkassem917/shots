88ca5f36d4a8d02118b5c3799a4ea15d381b80f9
Matthew Duffield <duffieldml@gmail.com>
2021-03-22 19:20:41 -0400

    Changed user data for age

--- models/user.js
+++ models/user.js
@@ -30,7 +30,7 @@ module.exports = function(sequelize, DataTypes) {
       allowNull: false
     },
     age: {
-      type: DataTypes.INTEGER,
+      type: DataTypes.BOOLEAN,
       allowNull: false
     },
     essential_worker: {
@@ -38,18 +38,5 @@ module.exports = function(sequelize, DataTypes) {
       defaultValue: false
     }
   });
-  // Creating a custom method for our User model. This will check if an unhashed password entered by the user can be compared to the hashed password stored in our database
-  // User.prototype.validPassword = function(password) {
-  //   return bcrypt.compareSync(password, this.password);
-  // };
-  // Hooks are automatic methods that run during various phases of the User Model lifecycle
-  // In this case, before a User is created, we will automatically hash their password
-  // User.addHook("beforeCreate", user => {
-  //   user.password = bcrypt.hashSync(
-  //     user.password,
-  //     bcrypt.genSaltSync(10),
-  //     null
-  //   );
-  // });
   return User;
 };
