af661a0f01cdc1836ea5054b7d1e270804692af9
Jacqueline Ross <jacqueline.ross09@gmail.com>
2021-03-23 19:50:37 -0400

    route drafted
    getting an error

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,11 +1,12 @@
 const db = require("../models");
 
-module.exports = app => {
+module.exports = (app) => {
   // Home page (Sign-up Form)
   app.get("/", (req, res) => {
     res.render("index", { title: "vaccine" });
   });
 
+  //renders a customized page based on the users data
   app.post("/", (req, res) => {
     console.log(req.body);
     db.User.create({
@@ -14,19 +15,34 @@ module.exports = app => {
       last_name: req.body.last_name,
       state: req.body.state,
       age: req.body.age,
-      essential_worker: req.body.essential_worker
+      essential_worker: req.body.essential_worker,
     })
-      .then(user => {
+      .then((user) => {
         console.log(user.dataValues);
         res.render("userdata", {
           title: "UserData",
           person: user.dataValues,
-          key: process.env.COVIDACTNOW_API
+          key: process.env.COVIDACTNOW_API,
         });
       })
-      .catch(err => {
+      .catch((err) => {
         res.status(401).json(err);
       });
+  });
+
+  //testing this 
+  // Model.findAll({
+  //   attributes: [[models.sequelize.literal('CASE WHEN "field1" = true THEN 55 ELSE 23 END'), 'field3']]
+  // }
+  app.get("/look", (req, res) => {
+    db.User.findAll({
+      attributes: [
+        [
+          db.User.sequelize.literal('CASE WHEN "age" = true', 'elligible')
+          // db.User.sequelize.literal('CASE WHEN "essential_worker" = true'),
+        ],
+      ],
+    }).then((user) => res.render(user));
   });
 
   // will be deleted after app is done
@@ -38,35 +54,39 @@ module.exports = app => {
         last_name: "Ross",
         state: "MI",
         age: "30",
-        essential_worker: true
+        essential_worker: true,
       },
-      key: process.env.COVIDACTNOW_API
+      key: process.env.COVIDACTNOW_API,
     });
   });
 
+  //returning user
   app.get("/returning", (req, res) => {
     res.render("returninguser");
   });
 
+  // returning user enters email and brings to user data
   app.get("/returning/:email", (req, res) => {
     db.User.findOne({
       where: {
-        email: req.params.email
-      }
-    }).then(user => {
+        email: req.params.email,
+      },
+    }).then((user) => {
       console.log(user);
       res.render("userdata", {
         title: "UserData",
         person: user.dataValues,
-        key: process.env.COVIDACTNOW_API
+        key: process.env.COVIDACTNOW_API,
       });
     });
   });
 
+  //renders statedata handlebars - not sure were usign now
   app.get("/state", (req, res) => {
     res.render("statedata");
   });
 
+  //renders all people in one state
   app.get("/state/:state", (req, res) => {
     db.User.findAll({
       where: {
@@ -74,7 +94,7 @@ module.exports = app => {
       },
     }).then((user) => {
       console.log(user);
-      res.render("vaccinedata", {person: user});
+      res.render("vaccinedata", { person: user });
     });
   });
 };
